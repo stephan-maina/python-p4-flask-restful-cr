540d8fc17bb76820bf45d8be73b4d616618fc99f
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-25 00:23:14 +0300

    Completed Lab

--- server/app.py
+++ server/app.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, make_response
-from flask_migrate import Migrate
-from flask_restful import Api, Resource
-
-from models import db, Newsletter
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newsletters.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newsletter.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
-migrate = Migrate(app, db)
-db.init_app(app)
+db = SQLAlchemy(app)
 
-api = Api(app)
+# Define your models (in models.py)
 
-class Home(Resource):
-    pass
+# Define your routes here
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(debug=True)
